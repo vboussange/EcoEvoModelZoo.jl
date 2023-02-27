25a0c27035c8701f07ee183d2df7ca2fe9edc1d1
Victor <bvictor@ethz.ch>
2023-02-27 11:29:18 +0100

    Boussange et al seems to work

--- src/Boussange_et_al/Boussange_et_al.jl
+++ src/Boussange_et_al/Boussange_et_al.jl
@@ -1,15 +1,18 @@
-using ParametricModels
-using UnPack
-using DocStringExtensions
-using Statistics
+using DiffEqOperators
 using Graphs
 
-# this function is called to evaluate the convolution of local u and alpha evaluated at S[i]
-function int_u(u::T, p) where T <: AbstractArray
-    @unpack N,S,dS = p
-    C = 0.5f0 * (u[1] + u[N])
-    C += sum(u[2:N-1])
+# this function is called to evaluate the convolution of local u and alpha evaluated at phen_space[i]
+function int_competition_fn(model, u, x, p) where T <: AbstractArray
+    @unpack phen_space = model
+    dS = phen_space[2]-phen_space[1]
+    C = 0.5f0 * (competition_fn(u[1], x, phen_space[1], p) + competition_fn(u[end], x, phen_space[end], p))
+    C += sum(competition_fn.(u[2:end-1], x, phen_space[2:end-1], Ref(p)))
     return C*dS
+end
+
+function rw_laplacian_matrix(g::AbstractGraph)
+    L = laplacian_matrix(g,dir=:out) ./ outdegree(g)
+    replace!(L,NaN=>0)
 end
 
 """
@@ -19,90 +22,92 @@ This model is inspired from [Boussange & Pellissier. 2022](https://www.nature.co
 
 # Example
 ```julia
+# Testing model
+using ParametricModels
+using UnPack
+using DocStringExtensions
+using Statistics
+using ComponentArrays
+using Distributions
+using OrdinaryDiffEq
+using UnPack
+## Defining phenotypic space and spatial graph
+M = 7
+g = star_graph(M)
+
+rS = 1f0
+dS = 0.02f0
+phen_space = collect(range(-rS,rS,step=dS)) #grid
+
+##
+# Defining birth and competition functions
+soptim = 0.5f0 * [-1,1,-1,1,-1,1,-1]
+birth_fn(x, s, p) = max(0f0, 1f0 - (soptim[x] - s)^2)
+competition_fn(u_xs, x, s, p) = u_xs ./ p.K
 
 ## Defining parameters
-mu= 0.1f0
-K1 = 1f0 #scaled to 1 for decreasing numerical errors
-M = 7;
-dS = 0.02f0;
-rS = 1f0;
 σ_mu = 5f-2;
-c = 1f0 / K1;
-tend = 1000f0
-tspan = (0f0,tend)
+mu = 0.1f0
+m = 0.1
+K = 1.
+
+p = ComponentArray(σ_mu = [σ_mu],
+                    mu = [mu],
+                    m = [m],
+                    K = [K])
+
 
 ## rest of the simulation
-g = star_graph(M)
-S = collect(range(-rS,rS,step=dS)) #grid
-N = length(S)
-Δ_s = Array(σ_mu^2/2 *mu * CenteredDifference{1}(2, 2, dS, N))[:,2:end-1]
-X = 1:M
-soptim = 0.5f0 * Float32[-1,1,-1,1,-1,1,-1]
-u0 = vcat([K1 .* pdf.(Normal(so,σ_mu),S') for so in soptim]...)
+tend = 1000f0
+tspan = (0f0,tend)
+tsteps = (tspan[1]):1.:(tspan[end])
+u0 = vcat([K .* pdf.(Normal(so,σ_mu),phen_space') for so in soptim]...)
 
-B(x,s) = max(0f0,1f0 - (soptim[x] - s)^2)
+mp = ModelParams(;p,
+                tspan,
+                u0,
+                alg=Tsit5(),
+                saveat = tsteps)
+
+model = EcoEvoGraph(mp, g, phen_space, birth_fn, competition_fn)
+
+simulate(model)
 ```
 """
-Base.@kwdef struct EcoEvoGraph{MP,G,BFN,CFN} <: AbstractModel
+Base.@kwdef struct EcoEvoGraph{MP,G,PS,DS,DX,BFN,CFN} <: AbstractModel
     mp::MP
     g::G # graph
+    phen_space::PS
+    Δ_s::DS
+    Δ_x::DX
     birth_fn::BFN # number of resources
     competition_fn::CFN # supply rates
 end
 
-function (m::EcoEvoGraph)(du,u,p,t)
-    @unpack N, M, S, dS, X, Δ_s, Δ_x = m
-    @unpack m = p
+function (model::EcoEvoGraph)(du,u,p,t)
+    @unpack phen_space, Δ_s, Δ_x, g, birth_fn, competition_fn = model
+    @unpack m, σ_mu, mu = p
 
     u[u[:,:] .< eps()] .= 0
-    B_i = B.(1:length(g), S')
-    Δ_x = m * rw_laplacian_matrix(g)
+    B_mat = birth_fn.(1:nv(g), phen_space', Ref(p))
 
-    for i in 1:M
+    for i in 1:nv(g) # iterating over graph nodes
         u_i = @view u[i,:]
-        C_i = int_u((u_i), p)
+        C_i = int_competition_fn(model, u_i, i, p)
         # here there is no b (1-m) because the -m is included in the laplacian
-        du[i,:] .= u_i .* (B_i[i,:] .- C_i / K1  )
+        du[i,:] .= u_i .* (B_mat[i,:] .- C_i )
     end
-    # @show size(u)
-    # @show size(Δ_x')
-    # @show size(B_i)
-    # @show size(u .* B_i)
-    mul!(lux, Δ_x', (u .* B_i))
-    mul!(lus, u .* B_i, Δ_s )
+
+    lux = m .* Δ_x' * (u .* B_mat)
+    lus = σ_mu.^2/2 .* mu .* (u .* B_mat) * Δ_s 
     @. du = du - lux + lus
     return du
 end
 
-## Parameters used
-mu= 0.1f0
-K1 = 1f0 #scaled to 1 for decreasing numerical errors
-M = 7;
-dS = 0.02f0;
-rS = 1f0;
-σ_mu = 5f-2;
-c = 1f0 / K1;
-tend = 1000f0
-tspan = (0f0,tend)
-
-## rest of the simulation
-g = star_graph(M)
-S = collect(range(-rS,rS,step=dS)) #grid
-N = length(S)
-Δ_s = Array(σ_mu^2/2 *mu * CenteredDifference{1}(2, 2, dS, N))[:,2:end-1]
-X = 1:M
-soptim = 0.5f0 * Float32[-1,1,-1,1,-1,1,-1]
-u0 = vcat([K1 .* pdf.(Normal(so,σ_mu),S') for so in soptim]...)
-
-B(x,s) = max(0f0,1f0 - (soptim[x] - s)^2)
-
-lux = similar(u0);
-lus = similar(u0);
-
-# non linear term, i.e. the logistic summand of the IDE
-
-
-p_default = Dict{String,Any}()
-@pack! p_default = M,S,dS,N,Δ_s,σ_mu,X
-
-    #test
+function EcoEvoGraph(mp::ModelParams, g, phen_space, birth_fn, competition_fn)
+    dS = phen_space[2]-phen_space[1]
+    N = length(phen_space)
+    Δ_s = Array(CenteredDifference{1}(2, 2, dS, N))[:,2:end-1]
+    Δ_x = rw_laplacian_matrix(g)
+    EcoEvoGraph(mp, g, phen_space, Δ_s, Δ_x, birth_fn, competition_fn)
+end
